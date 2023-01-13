452622f17c68e7f560c3dee2ca79ba7fe159e7db
shizc4 <shizc4@lenovo.com>
2023-01-13 10:59:54 +0800

    日志体系

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# java-log-framework-tutorial
-java日志框架讲解
+# java日志框架讲解
+# 日志体系讲解
+下图是SlF4j日志的体系图。
+
+![](https://itlab1024-1256529903.cos.ap-beijing.myqcloud.com/202301131057384.png)
+
+简单说明下：
---
+++ slf4j-unbound/src/main/java/com/itlab1024/log/Main.java
@@ -0,0 +1,15 @@
+package com.itlab1024.log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.debug("debug");
+        logger.info("info");
+        logger.warn("warn");
+        logger.trace("trace");
+        logger.error("error");
+    }
+}
